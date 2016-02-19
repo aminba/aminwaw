a7bbe1918051a040ab462f32f4ea7a329206df3d
aminba <elnabarekat@gmail.com>
2016-02-19 09:18:09 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {133202010,128132090},
+    sudo_users = {133202010,128132090,92112166},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
